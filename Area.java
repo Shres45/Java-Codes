190475e8384d27fec93cdc4b227f0623ee665560
Shrestha Mukherjee <shresthamukherjee45@gmail.com>
2026-03-26 14:39:51 +0530

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,12 +1,30 @@
-public class Area {
-    void display(double length, double width){
-        double area = length * width;
-        System.out.println("Area of Rectangle is : "+area);
-        double perimeter = 2*(length+width);
-        System.out.println("Perimeter of Rectangle is : "+perimeter);
+class Point{
+    int a,b;
+    Point(int a,int b){
+        this.a= a;
+        this.b= b;
+
     }
-    public static void main(String [] args){
-        Area obj = new Area();
-        obj.display(12,13);
+    void displayp(){
+        System.out.print("Center point is(" + a + "," + b + ")");
     }
+}
+    class Circle{
+    double r;
+    Point c;
+    Circle(double r, int a, int b){
+        this.r=r;
+        c=new Point(a,b);
+    }
+    void displaya(){
+        double area= Math.PI*r*r;
+        c.displayp();
+        System.out.print("Area of the circle is"+ area);
+    }
+}
+public class Area{
+        public static  void main(String[] args){
+            Circle c = new Circle(3,4,5);
+            c.displaya();
+        }
 }
